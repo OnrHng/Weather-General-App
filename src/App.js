20066cc6920aaa8c0892170688557df55e8848fa
OnrHng <hangul07@hacettepe.edu.tr>
2020-01-23 01:49:13 +0100

    cors error

--- src/App.js
+++ src/App.js
@@ -47,7 +47,6 @@ class App extends Component {
             key: "5601158bc86d44c48da97c8bbe8cafb2"
           }
           headers: {
-                'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
                 'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
             }
