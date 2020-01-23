57e1d339886e70d433a06fb180aa351ff1ecc7f0
OnrHng <hangul07@hacettepe.edu.tr>
2020-01-23 20:02:53 +0100

    Updates

---
+++ precache-manifest.3f10c4bbbb519e82945ca35f21d303aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81340cec916a74f3990f30cda89352a4",
+    "url": "/Weather-General-App/index.html"
+  },
+  {
+    "revision": "3befc15c700cfb316c4f",
+    "url": "/Weather-General-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bef11f36fdcfec784604",
+    "url": "/Weather-General-App/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3befc15c700cfb316c4f",
+    "url": "/Weather-General-App/static/js/2.21ad6c90.chunk.js"
+  },
+  {
+    "revision": "bef11f36fdcfec784604",
+    "url": "/Weather-General-App/static/js/main.27ceeca4.chunk.js"
+  },
+  {
+    "revision": "9a0555db7e5175ef3869",
+    "url": "/Weather-General-App/static/js/runtime-main.58a154df.js"
+  }
+]);
